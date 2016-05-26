5e92f01c5f15923ba03bbf54e8fbe34a83f56c0a
Sid <sid26@ucla.edu>
2016-05-26 07:23:15 -0700

    added recent tracks sorting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 import curryfm from 'curryfm';
 import jsonfile from 'jsonfile';
 import spotifier from 'spotifier';
-import youtuber from 'youtuber'
+import youtuber from 'youtuber';
 import firebase from 'firebase';
 
 /* API INITIALIZERS */
@@ -70,14 +70,11 @@ const recentTrackInfo = (track) => {
   const title = getKey(null, ['name'])(track);
   const artist = getKey(null, ['artist', '#text'])(track);
   const epoch = getKey(null, ['date', 'uts'])(track);
-  const timestamp = epoch ? moment.unix(epoch)
-   .fromNow() : null;
-  const active = getKey(false, ['@attr', 'nowplaying'])(track) ? true : false;
+  const active = getKey(false, ['@attr', 'nowplaying'])(track) ? true : null;
   return {
    title,
    artist,
    epoch,
-   timestamp,
    active
   };
 };
@@ -97,8 +94,30 @@ const topTrackInfo = (track) => {
 
 /* TRANSFORMS */
 
-const TransformRecentTracks = R.compose(R.map(recentTrackInfo), getKeyRecentTracks);
-const TransformTopTracks = R.compose(R.map(topTrackInfo), getKeyTopTracks);
+const transformRecentTracks = R.compose(R.map(recentTrackInfo), getKeyRecentTracks);
+
+const transformTopTracks = R.compose(R.map(topTrackInfo), getKeyTopTracks);
+
+const sortRecentTracks = (tracks) => {
+  const sortByEpoch = R.pipe(R.sortBy(R.prop("epoch")), R.reverse);
+  const findActiveTrack = (track) => {
+    if(R.pathOr(false, ["active"], track)) {
+      return true;
+    }
+    else return false;
+  };
+  let active_track = R.filter(findActiveTrack, tracks);
+  const sorted_tracks = (() => {
+    const sorted = sortByEpoch(tracks);
+    if (!R.isEmpty(active_track)) {
+      const sorted_without_active = R.dropLast(1, sorted);
+      const sorted_with_active = R.insert(0, R.head(active_track), sorted_without_active);
+      return sorted_with_active;
+    }
+    return sorted;
+  })();
+  return sorted_tracks;
+};
 
 /* CURRYFM HELPERS */
 
@@ -119,17 +138,17 @@ let getSpotifyMetaData = (client, primaryCallback, secondaryCallback, tracks) =>
 	const length = R.length(tracks);
 	let fetched = 0;
 	let spotified_tracks = [];
-  const max_retries = 3;
+  const min_retries = 3;
   let retries = 0;
 	let getData = (primaryCallback, track) => {
 		const { title, artist } = track;
 		client.findBestMatch({ title, artist }, (err, result) => {
 			if (err) {
 				console.error(track.title, JSON.stringify(err));
-        if (retries < max_retries) {
+        if (retries < min_retries) {
           retries++;
           getData(primaryCallback, track);
-        } else if (retries === max_retries) {
+        } else if (retries === min_retries) {
           retries = 0;
           fetched++;
           primaryCallback({}, track, spotified_tracks);
@@ -175,15 +194,15 @@ let mergeYoutubeMetaDataAnd = R.curry((client, secondaryCallback, tracks) => {
   const length = R.length(tracks);
   let fetched = 0;
   let youtubed_tracks = [];
-  const max_retries = 3;
+  const min_retries = 3;
   let retries = 0;
   let pushToTracks = (track, err) => {
     if(err) {
       console.error(track.title, JSON.stringify(err, null, 1));
-      if (retries < max_retries) {
+      if (retries < min_retries) {
         retries++;
         client(pushToTracks, track);
-      } else if (retries === max_retries) {
+      } else if (retries === min_retries) {
         retries = 0;
         fetched++;
         youtubed_tracks.push(track);
@@ -208,6 +227,7 @@ const db = firebase.database();
 const top_tracks_monthly_ref = db.ref("toptracks/monthly");
 const top_tracks_weekly_ref = db.ref("toptracks/weekly");
 const top_tracks_quarterly_ref = db.ref("toptracks/quarterly");
+const recent_tracks_ref = db.ref("recenttracks");
 // ref.orderByChild("rank")
 // 	.on("child_changed", (snapshot) => {
 // 		console.log(snapshot.val());
@@ -227,7 +247,7 @@ const setTracksInFirebase = R.curry((ref, tracks) => {
 // const prependSpace = (val) => " " + val;
 // const nicelyLog = R.compose(console.log, R.curry(prettyJSON)(1));
 // const logTopTracks = R.compose(R.map(console.log), TransformTopTracks);
-// const logRecentTracks = R.compose(R.map(console.log), TransformRecentTracks);
+// const logRecentTracks = R.compose(R.map(console.log), transformRecentTracks);
 // const logTitles = R.compose(console.log, recentTrackInfo);
 // const pushToArray = R.curry((array, item) => {
 // 	array.push(item);
@@ -263,28 +283,37 @@ const setTracksInFirebase = R.curry((ref, tracks) => {
 // 		});
 // 	}
 // };
-const callEveryHour = callEvery("01:00:00");
+const callEveryTwoHours = callEvery("02:00:00");
+const callEveryFiveSeconds = callEvery("00:00:05");
 
-const main_timer = callEveryHour(() => {
+const recenttracks_timer = callEveryFiveSeconds(() => {
+  getRecentTracks(getParams({
+    limit: 5
+  }), (result) => {
+    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(R.pipe(sortRecentTracks, setTracksInFirebase(recent_tracks_ref))))(transformRecentTracks(result));
+  })
+});
+
+const toptracks_timer = callEveryTwoHours(() => {
 	getTopTracks(getParams({
 		limit: 10,
 		page: 1,
 		period: "1month"
 	}), (result) => {
-    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(setTracksInFirebase(top_tracks_monthly_ref)))(TransformTopTracks(result));
+    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(setTracksInFirebase(top_tracks_monthly_ref)))(transformTopTracks(result));
 	});
   getTopTracks(getParams({
     limit: 10,
     page: 1,
     period: "7day"
   }), (result) => {
-    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(setTracksInFirebase(top_tracks_weekly_ref)))(TransformTopTracks(result));
+    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(setTracksInFirebase(top_tracks_weekly_ref)))(transformTopTracks(result));
   });
   getTopTracks(getParams({
     limit: 10,
     page: 1,
     period: "3month"
   }), (result) => {
-    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(setTracksInFirebase(top_tracks_quarterly_ref)))(TransformTopTracks(result));
+    mergeSpotifyMetaDataAnd(mergeYoutubeMetaDataAnd(setTracksInFirebase(top_tracks_quarterly_ref)))(transformTopTracks(result));
   });
 });
